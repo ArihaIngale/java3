9a297829d48b98fb1a2979e33a16b63383f969db
ArihaIngale <arihaingale220705@gmail.com>
2025-04-23 14:58:05 +0530

    save

--- demo.java
+++ demo.java
@@ -6,8 +6,11 @@ public class demo {
         int b=5;
         int sum=a+b;
         int sub=a-b;
+        int mul=a*b;
+        int div=a/b;
         System.out.println("sum is :"+sum);
         System.out.println("subtraction is :"+sub);
+        System.out.println("multiplication is :"+mul);
+        System.out.println("division is :"+div);
     }
-
-}
+} 
